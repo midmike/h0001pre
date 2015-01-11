07876a2710206859eba7e024cbf45043bccb5764
devcoo.group@gmail.com <devcoo.group@gmail.com@b88bea73-d6ee-b275-aace-549c85457c3e>
2015-01-11 06:16:28 +0000

    [mike] solved click back and result page not found, reason cause of ios homescreen app open with safari. Put attribute "id='back'" to skip that.

--- trunk/index.php
+++ trunk/index.php
@@ -4,12 +4,14 @@
 function checkMobileApp(){
 	var a=document.getElementsByTagName("a");
 	for(var i=0;i<a.length;i++)
-	{
-	    a[i].onclick=function()
-	    {
-	        window.location=this.getAttribute("href");
-	        return false
-	    }
+	{ 
+		if(a[i].id != "back" ) {
+		    a[i].onclick=function()
+		    {
+		        window.location=this.getAttribute("href");
+		        return false;
+		    }
+		}
 	}
 }
 </script>
