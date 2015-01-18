08eb868214da1fc537383cc212fe24ead0dc0a63
devcoo.group <devcoo.group@gmail.com>
2015-01-18 05:39:40 +0000

    [mike] add project export file in excel, word, pdf, and html that use with php report maker

--- public/User/pageViewTable.php
+++ public/User/pageViewTable.php
@@ -5,7 +5,7 @@
 	});
 	function datatable() {
 		if ($('#datatable-user').length > 0){
-			$('#datatable-user').dataTable( {
+			$('#datatable-user').DataTable( {
 				"bAutoWidth": false,
 				"language": {
 		            "lengthMenu": "<?php i18n::getLabel("message.user.manageuser.lengthmenu.0");?> _MENU_ <?php i18n::getLabel("message.user.manageuser.lengthmenu.1");?>",
@@ -58,6 +58,15 @@
 		}
 		myform.submit();
 	}
+	function exportFile(exportType) {
+		var myform = document.getElementById ('user_datatable_form');
+		var table = $('#datatable-user').DataTable();
+		var pageno = table.page();
+		var pageLen = table.page.len();
+		myform.action = "<?php echo Tool::getCurrentHost()?>/report/user_reportsmry.php?pageno="
+			 + pageno + "&grpperpage=" + pageLen + "&order=name&ordertype=ASC&export=" + exportType;
+		myform.submit();
+	}
 </script>
 <div class="container-fluid">
 	<!-- Begin page heading -->
@@ -68,47 +77,72 @@
 		<div class="the-box rounded">
 			<div class="the-box rounded form-group">
 				<div class="col-xs-12 col-md-2">
-					<a href="javascript:void(0);open('<?php echo CREATE?>');" class="btn btn-success btn-rounded-lg col-md-12 col-xs-12">
-						<i class="fa fa-plus"></i><?php i18n::getLabel("message.user.manageuser.button.new");?>
+					<a href="javascript:void(0);open('<?php echo CREATE?>');"
+						class="btn btn-success btn-rounded-lg col-md-12 col-xs-12"> <i
+						class="fa fa-plus"></i><?php i18n::getLabel("message.user.manageuser.button.new");?>
 					</a>
 				</div>
 				<div class="col-xs-12 col-md-2">
-					<a href="javascript:void(0);open('<?php echo EDIT?>');" class="btn btn-success btn-rounded-lg col-md-12 col-xs-12">
-						<i class="fa fa-pencil"></i><?php i18n::getLabel("message.user.manageuser.button.edit");?>
+					<a href="javascript:void(0);open('<?php echo EDIT?>');"
+						class="btn btn-success btn-rounded-lg col-md-12 col-xs-12"> <i
+						class="fa fa-pencil"></i><?php i18n::getLabel("message.user.manageuser.button.edit");?>
 					</a>
 				</div>
 				<div class="col-xs-12 col-md-3">
-					<a href="javascript:void(0);open('<?php echo SHOWHIDE?>');" class="btn btn-success btn-rounded-lg col-md-12 col-xs-12">
-						<i class="fa fa-eye"></i><?php i18n::getLabel("message.user.manageuser.button.show");?>
+					<a href="javascript:void(0);open('<?php echo SHOWHIDE?>');"
+						class="btn btn-success btn-rounded-lg col-md-12 col-xs-12"> <i
+						class="fa fa-eye"></i><?php i18n::getLabel("message.user.manageuser.button.show");?>
 					</a>
 				</div>
-				<a href="javascript:void(0);open('<?php echo REFRESH?>');" class="btn" ><i class="fa fa-refresh"></i></a>
+				<div class="col-xs-12 col-md-2">
+					<a href="javascript:void(0);exportFile('<?php echo EXCEL?>');"
+						class="btn btn-primary btn-rounded col-md-3 col-xs-12"> <i
+						class="fa fa-file-excel-o"></i>
+					</a> <a href="javascript:void(0);exportFile('<?php echo WORD?>');"
+						class="btn btn-info btn-rounded col-md-3 col-xs-12"> <i
+						class="fa fa-file-word-o"></i>
+					</a> <a href="javascript:void(0);exportFile('<?php echo PDF?>');"
+						class="btn btn-danger btn-rounded col-md-3 col-xs-12"> <i
+						class="fa fa-file-pdf-o"></i>
+					</a> <a href="javascript:void(0);exportFile('<?php echo HTML?>');"
+						class="btn btn-default btn-rounded col-md-3 col-xs-12"> <i
+						class="fa fa-html5"></i>
+					</a>
+				</div>
+				<a href="javascript:void(0);open('<?php echo REFRESH?>');"
+					class="btn"><i class="fa fa-refresh"></i></a>
 			</div>
-		
+
 			<!-- End Button -->
 			<!-- Pop up -->
 			<div id="text-popup-html" class="white-popup mfp-with-anim mfp-hide">
 			</div>
 			<!-- End Pop up -->
-			<input type="hidden" name="cache_val" value="<?php User::CACHE_SHOW?>"/>
+			<input type="hidden" name="cache_val"
+				value="<?php User::CACHE_SHOW?>" />
 			<!-- BEGIN DATA TABLE -->
 			<div class="the-box">
 				<div class="table-responsive">
-				<table class="table table-th-block table-success table-striped table-hover" data-click-to-select="true" id="datatable-user">
-					<thead class="the-box dark full">
-						<tr>
-							<th width="3px"></th>
-							<th><?php i18n::getLabel("message.user.register.fullname");?></th>
-							<th><?php i18n::getLabel("message.user.manageuser.table.status");?></th>
-							<th><?php i18n::getLabel("message.user.register.address");?></th>
-							<th><?php i18n::getLabel("message.user.register.phone");?></th>
-							<th><?php i18n::getLabel("message.user.manageuser.table.username");?></th>
-						</tr>
-					</thead>
-				</table>
-				</div><!-- /.table-responsive -->
-			</div><!-- /.the-box .default -->
+					<table
+						class="table table-th-block table-success table-striped table-hover"
+						data-click-to-select="true" id="datatable-user">
+						<thead class="the-box dark full">
+							<tr>
+								<th width="3px"></th>
+								<th><?php i18n::getLabel("message.user.register.fullname");?></th>
+								<th><?php i18n::getLabel("message.user.manageuser.table.status");?></th>
+								<th><?php i18n::getLabel("message.user.register.address");?></th>
+								<th><?php i18n::getLabel("message.user.register.phone");?></th>
+								<th><?php i18n::getLabel("message.user.manageuser.table.username");?></th>
+							</tr>
+						</thead>
+					</table>
+				</div>
+				<!-- /.table-responsive -->
+			</div>
+			<!-- /.the-box .default -->
 			<!-- END DATA TABLE -->
 		</div>
 	</form>
-</div><!-- /.container-fluid -->
+</div>
+<!-- /.container-fluid -->
