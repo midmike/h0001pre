ed7ae0f7756884ffe74756e95389718be32d6882
devcoo.group@gmail.com <devcoo.group@gmail.com@b88bea73-d6ee-b275-aace-549c85457c3e>
2015-05-17 15:27:33 +0000

    [Mike] Add Vaadin Project

---
+++ AgencyFlight/agencyflight-core/src/test/java/test/user/TestUser.java
@@ -0,0 +1,30 @@
+package test.user;
+
+
+public class TestUser {
+
+//	@SuppressWarnings("resource")
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+//		UserService userManager = (UserService) ctx.getBean("userServiceImp");
+//		AddressService addressService = (AddressService) ctx.getBean("addressServiceImp");
+////
+////		List<User> list = userManager.findAllUsers();
+////		// System.out.println("User count: " + list.size());
+////
+//		User user = new User();
+//		user.setName("John Doe");
+//		userManager.insertUser(user);
+//		System.out.println("User inserted!");
+//		
+//		List<Address> listAdd=addressService.findAll();
+//		System.out.println("Address count: " + listAdd.size());
+//
+//		
+////
+//		list = userManager.findAllUsers();
+//		System.out.println("User count: " + list.size());
+//	}
+
+}
