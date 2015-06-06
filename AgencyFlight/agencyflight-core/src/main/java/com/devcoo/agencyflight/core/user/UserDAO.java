41b54ceafb6614ff9064491836fb0a50074c7738
devcoo.group@gmail.com <devcoo.group@gmail.com@b88bea73-d6ee-b275-aace-549c85457c3e>
2015-06-06 04:54:08 +0000

    no use DAO

--- AgencyFlight/agencyflight-core/src/main/java/com/devcoo/agencyflight/core/user/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package com.devcoo.agencyflight.core.user;
-
-import org.springframework.data.repository.Repository;
-
-
-public interface UserDAO extends Repository<User,Integer> {
-	User getAuthoriseUser(String name,String password);
-}
